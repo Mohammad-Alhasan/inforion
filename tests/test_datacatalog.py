13c16b2255d155e3084f9c6d46e34bcb8ec04475
Amier Haider <amier.haider@fellow-consulting.de>
2020-06-25 17:12:34 +0200

    Commented a function to run the pipeline.

--- tests/test_datacatalog.py
+++ tests/test_datacatalog.py
@@ -8,8 +8,10 @@ def test_get_datacatalog_ping():
     assert datacatalog.get_datacatalog_ping(base_url, ion_file).status_code == 200
 
 
+"""
 def test_delete_datacatalog_object():
     base_url = 'https://mingle-ionapi.eu1.inforcloudsuite.com/FELLOWCONSULTING_DEV'
     ion_file = 'credentials/credentials.ionapi'
     object_name = 'MySampleCSV'
     assert datacatalog.delete_datacatalog_object(object_name, base_url, ion_file).status_code == 200
+"""
