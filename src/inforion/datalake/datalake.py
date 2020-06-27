66e2e26f18de820667ed2ed33b6567cd811b5275
Daniel Jordan <daniel.jordan@fellow-consulting.de>
2020-06-27 22:49:09 +0200

    small fix

--- src/inforion/datalake/datalake.py
+++ src/inforion/datalake/datalake.py
@@ -49,8 +49,8 @@ def post_to_data_lake(url, ionfile,imslid, inputfile, schema):
 
 def get_datalake_ping(base_url, ion_file):
     ping_url_path = '/IONSERVICES/datalakeapi/v1/status/ping'
-    config = load_config(ion_file)
-    token = login(base_url, config)
+    config = inforlogin.load_config(ion_file)
+    token = inforlogin.login()
     headers = {
         "accept": "application/json",
         "Authorization": "Bearer {}".format(token['access_token'])
