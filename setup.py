fec175b80c1683c5ee8f4cdfc233401f0e14e6a1
Daniel Jordan <daniel.jordan@fellow-consulting.de>
2020-06-30 15:18:02 +0200

    higher thank 3.7

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ def load_requirements(fname):
     reqs = parse_requirements(fname, session="test")
     return [str(ir.req) for ir in reqs]
 
-if sys.version_info < (3, 6):
-    print("Python 3.6 or newer is required", file=sys.stderr)
+if sys.version_info < (3, 7):
+    print("Python 3.7 or newer is required", file=sys.stderr)
     sys.exit(1)
 
 # pylint: disable=w0613
