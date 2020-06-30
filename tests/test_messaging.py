0ce1d1faea0dad426980fcd6c51435fcae58e166
Roberto Metz <robertometz@robertometz.com>
2020-06-29 21:05:20 -0400

    Update messaging

--- tests/test_messaging.py
+++ tests/test_messaging.py
@@ -1,2 +1,74 @@
 import pytest
-from inforion.messaging import messaging
+
+from inforion.datacatalog.datacatalog import post_datacatalog_object, ObjectSchemaType, delete_datacatalog_object
+from inforion.ionapi.model import inforlogin
+from inforion.messaging.messaging import get_messaging_ping, post_messaging_v2_multipart_message
+
+
+def test_get_messaging_ping():
+    inforlogin.load_config('credentials/credentials.ionapi')
+    token = inforlogin.login()
+    assert get_messaging_ping(token).status_code == 200
+
+
+# @pytest.mark.skip()
+def test_post_messaging_v2_multipart_message():
+    inforlogin.load_config('credentials/credentials.ionapi')
+    token = inforlogin.login()
+
+    # create Document Schema
+    object_name = 'CSVSchema2'
+    schema = {
+        "$schema": "http://json-schema.org/draft-06/schema#",
+        "$id": "http://schema.infor.com/json-schema/{}.json".format(object_name),
+        "title": object_name,
+        "type": "object",
+        "dialect": {
+            "separator": ",",
+            "skipLines": 1,
+            "headerLine": 1
+        },
+        "properties": {
+            "ID": {
+                "type": "integer",
+                "x-position": 1,
+                "maximum": 9
+            },
+            "FIRST_NAME": {
+                "type": "string",
+                "x-position": 2,
+                "maxLength": 25
+            },
+            "LAST_NAME": {
+                "type": "string",
+                "x-position": 3,
+                "maxLength": 25
+            },
+            "COUNTRY": {
+                "type": "string",
+                "x-position": 4,
+                "maxLength": 25
+            }
+        }
+    }
+    properties = {
+
+    }
+    # assert post_datacatalog_object(token, object_name, ObjectSchemaType.DSV, schema, properties).status_code == 200
+
+    # post Document
+    parameter_request = {
+        "documentName": "{}".format(object_name),
+        "fromLogicalId": "lid://infor.ims.mongooseims",
+        "toLogicalId": "lid://default",
+        "encoding": "NONE",
+        "characterSet": "UTF-8"
+    }
+
+    with open("data/sample.csv", "rb") as file:
+        message_payload = file.read()
+
+    assert post_messaging_v2_multipart_message(parameter_request, message_payload) == 200
+
+    # delete Document Schema
+    assert delete_datacatalog_object(token, object_name).status_code == 200
